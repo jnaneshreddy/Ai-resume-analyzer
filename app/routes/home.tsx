97fe21518b16264034b32d2625735194205b7427
jnannesh reddy <jnaneshreddyv730@gmail.com>
2025-08-27 13:19:05 +0530

    created a responsive and good looking landing page for the application and nav bar with upload button

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Resumecard from "~/components/Resumecard";
+import { resumes } from "../../constants";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind BY SANSAI" },
+    { name: "description", content: "Samrt Resume analyzer" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+    <div className="page-heading py-10">
+      <h1>Track Your Applications and  Resume ratings</h1>
+      <h2>Review your submissions and check AI-powred feedback.</h2>
+    </div>
+   
+
+    {resumes.length>0 && (
+<div className="resumes-section">
+      {resumes.map((resume)=>(
+        <Resumecard key={resume.id} resume={resume}/>
+      ))}
+    </div>
+    )}
+     </section>
+  </main>;
 }
